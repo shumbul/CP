767950ba169f3f8e32745fbaca45fc418506e0dd
Shumbul Arifa <shumbul.181co152@nitk.edu.in>
2021-04-25 23:49:02 +0530

    leetcode

---
+++ Mixed/w1_count-element.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define intarr(n,arr,i)	int n,i; cin>>n; int arr[n]; for(i=0;i<n;i++) {cin>>arr[i];}
+#define FOR(i,a,b) for (int i=a;i<=b;i++)
+#define FORD(i,a,b) for (int i=a;i>=b;i--)
+
+void sum()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.in", "r", stdin);
+	freopen("output.in", "w", stdout);
+	freopen("error.in", "w", stderr);
+#endif
+}
+
+int32_t main()
+{
+	sum();
+
+	return 0;
+}
+
